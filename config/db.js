15d49814daf67d6d15c37a9421889dcc980ef7cb
Ritik Bansal <ritik.19b101185@abes.ac.in>
2025-03-04 01:10:13 +0530

    remote database setup

--- config/db.js
+++ config/db.js
@@ -1,17 +1,31 @@
 const { Sequelize } = require("sequelize");
 
 const dbHost = process.env.DB_HOST
+const dbPort = process.env.DB_PORT
 const dbName = process.env.DB_NAME
 const dbUser = process.env.DB_USER
 const dbPassword = process.env.DB_PASSWORD
 
-console.log("db credentials: ", dbHost, dbName, dbUser, dbPassword);
+console.log("db credentials: ", dbHost, dbPort, dbName, dbUser, dbPassword);
 
 
 const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
   host: dbHost,
+  port: dbPort,
   dialect: "mysql",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Required for some cloud-hosted DBs
+    },
+  },
   logging: false,
+  pool: {
+    max: 5, // Check if this is causing the issue
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 });
 
 sequelize.authenticate()
