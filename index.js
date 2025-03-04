3defb190a258cd3d06f41351379c492d842950a0
Ritik Bansal <ritik.19b101185@abes.ac.in>
2025-03-04 19:24:23 +0530

    hit and trial

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ const port = 3000;
 // app.use("/uploads", express.static(path.join(__dirname, "/tmp/uploads")));
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => res.json({
   "status":"ok",
